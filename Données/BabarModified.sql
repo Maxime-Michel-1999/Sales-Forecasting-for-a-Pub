130dfe7fb34f2d030a1925009dde9668388920c8
Maxime-Michel-1999 <maxime.michel@lafamillemichel.com>
2020-12-07 14:25:03 +0100

    report

--- Données/BabarModified.sql
+++ Données/BabarModified.sql
@@ -19,9 +19,16 @@ OR NAME = 'Pelforth' OR NAME = 'Skoll' OR NAME = 'BrewDog Punk IPA' OR NAME =
 OR NAME = 'Paix Dieu 33cL' OR NAME = 'San Miguel' OR NAME = 'Ambrée pinte' OR NAME
 = 'Ambrée demi');
 
-DROP TABLE IF EXISTS `Not Beer`;
-CREATE TABLE `High Degree Beer` AS SELECT babar_server_purchase.* FROM babar_server_purchase JOIN babar_server_product ON babar_server_purchase.product_id = babar_server_product.id 
+DROP TABLE IF EXISTS `Not_Beer`;
+CREATE TABLE `Not_Beer` AS SELECT babar_server_purchase.* FROM babar_server_purchase JOIN babar_server_product ON babar_server_purchase.product_id = babar_server_product.id 
 WHERE YEAR(TIMESTAMP) > 2013 AND YEAR(TIMESTAMP) < 2020  AND
-(NAME = 'Smirno' OR NAME = 'Pastis' OR NAME = 'Hard' OR NAME = 'Kir' OR NAME = 'Cocktail Hard' OR NAME = 'Shot' OR NAME = 'Rouge Pinte' OR NAME = 'Rouge Demi'
-OR NAME = 'Sangria' OR NAME = 'Granita Hard' OR NAME = 'Hard Qualite' OR NAME =
+(NAME = 'Smirnoff' OR NAME = 'Pastis' OR NAME = 'Hard' OR NAME = 'Kir' OR NAME = 'Cocktail Hard' OR NAME = 'Shot' OR NAME = 'Rouge Pinte' OR NAME = 'Rouge Demi'
+OR NAME = 'Sangria' OR NAME = 'Granita Hard' OR NAME = 'Hard Qualitée' OR NAME =
 'JagerBomb');
+
+DROP TABLE IF EXISTS `Special_Beer`;
+CREATE TABLE `Special_Beer` AS SELECT babar_server_purchase.* FROM babar_server_purchase JOIN babar_server_product ON babar_server_purchase.product_id = babar_server_product.id 
+WHERE YEAR(TIMESTAMP) > 2013 AND YEAR(TIMESTAMP) < 2020  AND
+(NAME = 'Desperados' OR NAME = "Cidre Demi" OR NAME = "Cidre Pinte" OR NAME = "Cidre Doux/Brut" OR NAME ="Cubanisto" OR NAME = "Corona"
+OR NAME = "Chimay Rouge" OR NAME = "Hoegaaden blanche" OR NAME ="Chimay Blanche" OR NAME ="Blanche Demi" OR NAME = "Blanche Pinte" OR NAME = "1664 Blanche" OR
+NAME = "Pecheresse" OR NAME = "Kriek" OR NAME = "Cherry chouffe" OR NAME = "Delirium rouge pinte");
--- Python/MLModels.py
+++ Python/MLModels.py
@@ -8,15 +8,23 @@ Created on Mon Dec  7 10:36:35 2020
 import HolidaysManager as FM
 import DataManager as DM
 import numpy as np 
+
+
 from sklearn.preprocessing import StandardScaler
 from sklearn import model_selection as ms
+from sklearn.metrics import mean_absolute_error
+from sklearn.neighbors import KNeighborsRegressor
 
 
 #Getting the data
 def GetData():
     X = FM.FeaturesData()
     y = DM.CreateSalesFrame()
+   
     y = y.drop(columns=['Year','Week Number'])
+    y = y.drop([304,305,306,307,308,309,310,311])
+    
+    return X, y
     
     #We create training and testing data that fit with sklearn package
 
@@ -28,5 +36,15 @@ def GetData():
     X_train = sc_X.fit_transform(X_train)
     X_test = sc_X.transform(X_test)
 
-    return [X_train,X_test,y_train,y_test]
+    #return [X_train,X_test,y_train,y_test]
     #Now We can try our models !
+
+def KNN_Regressor():
+    Data = GetData()
+    X_train = Data[0]
+    y_train = Data[2]
+    X_test = Data[1]
+    knn = KNeighborsRegressor(n_neighbors=10,n_jobs=4)
+    knn.fit(X_train,y_train)
+    y_pred = knn.predict(X_test)
+    return y_pred
